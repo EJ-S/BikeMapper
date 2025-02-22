4fefe99f308995d893a45ad18e6d756fb9546bf4
Elijah Spicer <eli.sushi.spicer@gmail.com>
2025-02-22 13:38:17 -0500

    add route saving

--- src/RoutePlanner.jsx
+++ src/RoutePlanner.jsx
@@ -7,10 +7,27 @@ import {
   useMapEvent,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 import { useNavigate } from "react-router-dom";
+import { Modal, Button, Box, TextField, Rating, Stack } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function RoutePlanner() {
   const [waypoints, setWaypoints] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [routeRating, setRouteRating] = useState(1);
+  const [routeName, setRouteName] = useState("");
   const map = useRef(null);
   let lat = 38.25;
   let long = -85.738;
@@ -33,6 +50,34 @@ function RoutePlanner() {
 
   const goHome = () => {
     navigate("/");
+  };
+
+  const openModal = () => {
+    setOpen(true);
+  };
+
+  const saveRoute = () => {
+    let dist = 0; // in meters
+    let center = [0, 0]; // in coordinates
+    for (var i = 0; i < waypoints.length - 1; i++) {
+      dist += L.latLng(waypoints[i].ltln).distanceTo(
+        L.latLng(waypoints[i + 1].ltln)
+      );
+    }
+    waypoints.map((location) => {
+      center[0] += location.ltln.lat;
+      center[1] += location.ltln.lng;
+    });
+    if (waypoints.length > 0) {
+      center[0] = center[0] / waypoints.length;
+      center[1] = center[1] / waypoints.length;
+    }
+    console.log("Route Distance:", dist);
+    console.log("Route Center:", center);
+    console.log("Route Name:", routeName);
+    console.log("Route Rating:", routeRating);
+    console.log("Points:", polyLinePositions);
+    setOpen(false);
   };
 
   const handleMarkerClick = (index) => {
@@ -66,37 +111,33 @@ function RoutePlanner() {
         <ClickLocater />
         <Polyline positions={polyLinePositions} color="blue" />
       </MapContainer>
+      <Button onClick={openModal} variant="contained">
+        Save this Route
+      </Button>
+      <Modal onClose={saveRoute} open={open}>
+        <Box sx={style}>
+          <Stack>
+            <TextField
+              id="route-name"
+              label="Route Name"
+              variant="outlined"
+              value={routeName}
+              onChange={(e) => setRouteName(e.target.value)}
+            />
+            <Rating
+              name="route-rating"
+              value={routeRating}
+              onChange={(event, newRouteRating) => {
+                setRouteRating(newRouteRating);
+              }}
+            />
+            <Button varient="contained" onClick={saveRoute}>
+              Save
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
     </div>
-    // <div>
-    //   <MapContainer
-    //     center={[lat, long]}
-    //     zoom={13}
-    //     style={{ height: "100%", width: "100%" }}
-    //     ref={map}
-    //   >
-    //     <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-    //     {waypoints.map((wp, index) => (
-    //       <Marker
-    //         key={index}
-    //         position={[wp.ltln.lat, wp.ltln.lng]}
-    //         eventHandlers={{ click: () => handleMarkerClick(index) }}
-    //       ></Marker>
-    //     ))}
-    //     {/* {waypoints.map((wp, index) => (
-    //       <Circle
-    //         key={index}
-    //         center={[wp.ltln.lat, wp.ltln.lng]}
-    //         radius={50}
-    //         pathOptions={{ color: "red", fillColor: "red", fillOpacity: 1 }}
-    //         eventHandlers={{
-    //           click: (event) => handleMarkerClick(event, index),
-    //         }}
-    //       ></Circle>
-    //     ))} */}
-    //     <ClickLocater />
-    //     <Polyline positions={polyLinePositions} color="blue" />
-    //   </MapContainer>
-    // </div>
   );
 }
 
